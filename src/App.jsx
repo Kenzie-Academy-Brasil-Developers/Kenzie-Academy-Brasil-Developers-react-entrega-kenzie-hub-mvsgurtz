4103636ee1f72d30ef555cdacf568d01c04db4fe
Marcos Vinicius Goulart <mvalvesgoulart@gmail.com>
2024-01-06 14:20:06 -0300

    Start LoginPage custom.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { LoginPage } from './Pages/LoginPage'
+import "./Styles/index.scss"
+
+function App() {
+
+
+  return (
+    <>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/LoginPage/index.jsx
@@ -0,0 +1,24 @@
+import style from "./style.module.scss";
+import Logo from "../../assets/Logo.svg";
+
+export const LoginPage = () => {
+  return (
+    <main>
+      <img src={Logo} alt="Logo" />
+      <div className={style.login__container}>
+        <h3>Login</h3>
+        <div className={style.login__info}>
+          <label htmlFor="Email">Email </label>
+          <input type="text" placeholder="Digite seu Email" />
+          <label htmlFor="Password">Senha </label>
+          <input type="text" placeholder="Digite a sua Senha" />
+          <button>Entrar</button>
+        </div>
+        <div className={style.register__acess}>
+          <p>Ainda nao possui uma conta?</p>
+          <button className={style.register__btn}>Cadastrar-se</button>
+        </div>
+      </div>
+    </main>
+  );
+};
